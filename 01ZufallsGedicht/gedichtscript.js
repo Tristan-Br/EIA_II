d52c4aacbe8935b85ba075f4f1fa5be5bf43f423
Tristan-Br <tristan.broghammer@hs-furtwangen.de>
2022-10-15 23:50:30 +0200

    Abgabe Aufgabe 1

--- 01ZufallsGedicht/gedichtscript.js
+++ 01ZufallsGedicht/gedichtscript.js
@@ -1,7 +1,25 @@
 var RandomPoem;
 (function (RandomPoem) {
-    let subjekt = ["Klaus", "Victor", "Hassan", "Kurt", "Herbert"];
-    let praedikat = ["feiert", "beendet", "klaut", "begutachtet", "liebt"];
-    let objekt = ["die Ukraine", "ansteigenden Leankonsum", "den berliner Fernsehturm", "Kurt", "Veganer"];
+    //Arrays
+    let subjekt = ["Klaus", "Victor", "Hassan", "Kurt", "Herbert", "Dimitri"];
+    let praedikat = ["feiert", "beendet", "klaut", "begutachtet", "liebt", "verkloppt"];
+    let objekt = ["die Ukraine", "ansteigenden Leankonsum", "den berliner Fernsehturm", "Kurt", "Veganer", "Major Monogwaem"];
+    //console.log(subjekt, praedikat, objekt);
+    //For-Schleife
+    for (let i = 6; i >= 1; i--) {
+        let randomsSatz = getVerse(subjekt, praedikat, objekt);
+        console.log(randomsSatz);
+    }
+    //Funktion-getVerse
+    function getVerse(_subjekt, _praedikat, _objekt) {
+        let randomSubjekt = Math.floor(Math.random() * _subjekt.length);
+        let randomPraedikat = Math.floor(Math.random() * _praedikat.length);
+        let randomObjekt = Math.floor(Math.random() * _objekt.length);
+        let verse = _subjekt[randomSubjekt] + " " + _praedikat[randomPraedikat] + " " + _objekt[randomObjekt] + ".";
+        _subjekt.splice(randomSubjekt, 1);
+        _praedikat.splice(randomPraedikat, 1);
+        _objekt.splice(randomObjekt, 1);
+        return verse;
+    }
 })(RandomPoem || (RandomPoem = {}));
 //# sourceMappingURL=gedichtscript.js.map
--- 01ZufallsGedicht/gedichtscript.ts
+++ 01ZufallsGedicht/gedichtscript.ts
@@ -1,7 +1,32 @@
 namespace RandomPoem {
+//Arrays
 
- let subjekt: string[] = ["Klaus", "Victor", "Hassan", "Kurt", "Herbert"];
- let praedikat: string[] = ["feiert", "beendet", "klaut", "begutachtet", "liebt"];
- let objekt: string[] = ["die Ukraine", "ansteigenden Leankonsum", "den berliner Fernsehturm", "Kurt", "Veganer"];
+ let subjekt: string[] = ["Klaus", "Victor", "Hassan", "Kurt", "Herbert", "Dimitri"];
+ let praedikat: string[] = ["feiert", "beendet", "klaut", "begutachtet", "liebt", "verkloppt"];
+ let objekt: string[] = ["die Ukraine", "ansteigenden Leankonsum", "den berliner Fernsehturm", "Kurt", "Veganer", "Major Monogwaem"];
+
+ //console.log(subjekt, praedikat, objekt);
+
+ //For-Schleife
+
+ for (let i: number = 6; i >= 1; i--) {let randomsSatz: string = getVerse(subjekt, praedikat, objekt);
+                                       console.log(randomsSatz);
 }
 
+//Funktion-getVerse
+
+ function getVerse (_subjekt: string[], _praedikat: string[], _objekt: string[]): string {
+
+    let randomSubjekt: number = Math.floor(Math.random() * _subjekt.length);
+    let randomPraedikat: number = Math.floor(Math.random() * _praedikat.length);
+    let randomObjekt: number = Math.floor(Math.random() * _objekt.length );
+ 
+    let verse: string = _subjekt[randomSubjekt] + " " + _praedikat[randomPraedikat] + " " + _objekt[randomObjekt] + ".";
+
+    _subjekt.splice(randomSubjekt, 1);
+    _praedikat.splice(randomPraedikat, 1);
+    _objekt.splice(randomObjekt, 1);
+
+    return verse;
+}
+}
