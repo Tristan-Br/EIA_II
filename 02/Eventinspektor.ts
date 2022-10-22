25c74bf8ffdf3eeaf8eb3277d80f7b9801903767
Tristan-Br <tristan.broghammer@hs-furtwangen.de>
2022-10-22 23:29:54 +0200

    Abgabe 02

---
+++ 02/Eventinspektor.ts
@@ -0,0 +1,57 @@
+namespace Eventinspector {
+
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(_event: Event): void {
+    document.addEventListener("mousemove", setInfoBox);
+    document.addEventListener("click", logInfo);
+    document.addEventListener("keyup", logInfo);
+
+    let body: HTMLElement = <HTMLElement>document.querySelector("body");
+
+    body.addEventListener("click", logInfo);
+    body.addEventListener("keyup", logInfo);
+
+    let div0: HTMLElement = <HTMLElement>document.getElementById("div0");
+
+    div0.addEventListener("click", logInfo);
+    div0.addEventListener("keyup", logInfo);
+
+    let div1: HTMLElement = <HTMLElement>document.getElementById("div1");
+
+    div1.addEventListener("click", logInfo);
+    div1.addEventListener("keyup", logInfo);
+    }
+
+    function setInfoBox(_event: MouseEvent): void {
+    let span: HTMLElement = <HTMLElement>document.querySelector("span");    
+    let x: number = _event.clientX;
+    let y: number = _event.clientY;
+    let target: HTMLElement = <HTMLElement>_event.target;  
+
+    span.style.top = y + 15 + "px";
+    span.style.left = x + 10 +  "px";
+    span.innerHTML = "X: " + x + " Y: " + y + " Objekt: " + target;
+    }
+
+    function logInfo(_event: Event): void {
+    console.log(_event.type);
+    console.log(_event.target);
+    console.log(_event.currentTarget);
+    console.log(_event);   
+    }
+
+    let button: HTMLElement = <HTMLElement>document.getElementById("button");
+    button.addEventListener("click", customEvent);
+
+    function customEvent(_event: Event ): void {
+    let newEvent: CustomEvent = new CustomEvent("C-137", {bubbles: true});
+    button.dispatchEvent(newEvent);
+
+    document.addEventListener("C-137", log);
+    }
+
+    function log(_event: CustomEvent): void {
+    console.log(_event);
+    }
+}
---
+++ 02EventInspector/Eventinspektor.js
@@ -0,0 +1,34 @@
+var Eventinspector;
+(function (Eventinspector) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        document.addEventListener("mousemove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        let body = document.querySelector("body");
+        body.addEventListener("click", logInfo);
+        body.addEventListener("keyup", logInfo);
+        let div0 = document.getElementById("div0");
+        div0.addEventListener("click", logInfo);
+        div0.addEventListener("keyup", logInfo);
+        let div1 = document.getElementById("div1");
+        div1.addEventListener("click", logInfo);
+        div1.addEventListener("keyup", logInfo);
+    }
+    function setInfoBox(_event) {
+        let span = document.querySelector("span");
+        let x = _event.clientX;
+        let y = _event.clientY;
+        let target = _event.target;
+        span.style.top = y + 15 + "px";
+        span.style.left = x + 10 + "px";
+        span.innerHTML = "X: " + x + " Y: " + y + " Objekt: " + target;
+    }
+    function logInfo(_event) {
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event);
+    }
+})(Eventinspector || (Eventinspector = {}));
+//# sourceMappingURL=Eventinspektor.js.map
