4812aeab7df4effac694ee29667f85fa4411d15c
Tristan-Br <tristan.broghammer@hs-furtwangen.de>
2022-10-29 23:52:41 +0200

    Js Update

--- 03Einkaufsliste/shoppingscript.js
+++ 03Einkaufsliste/shoppingscript.js
@@ -1,1 +1,27 @@
+var shoppingList;
+(function (shoppingList) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        let add = document.querySelector("button#button");
+        add.addEventListener("click", logAdd);
+        let check = document.querySelector("input#check");
+        check.addEventListener("click", logCheck);
+        let edit = document.querySelector("button#edit");
+        edit.addEventListener("click", logEdit);
+        let deletee = document.querySelector("button#deletee");
+        deletee.addEventListener("click", logDelete);
+    }
+    function logAdd() {
+        console.log("Item added");
+    }
+    function logCheck() {
+        console.log("Box checked");
+    }
+    function logEdit() {
+        console.log("Item edited");
+    }
+    function logDelete() {
+        console.log("Item deleted");
+    }
+})(shoppingList || (shoppingList = {}));
 //# sourceMappingURL=shoppingscript.js.map
