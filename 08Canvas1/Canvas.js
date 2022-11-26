39e1bcff92d5c032844d9af2cc8639d63fd72f4d
Tristan-Br <tristan.broghammer@hs-furtwangen.de>
2022-11-27 00:56:15 +0100

    Update

--- 08Canvas1/Canvas.js
+++ 08Canvas1/Canvas.js
@@ -17,9 +17,24 @@ var Canvas01;
         function randomNumber(_min, _max) {
             return Math.floor(Math.random() * _max) + _min;
         }
+        //Grid
+        crc2.beginPath();
+        for (let i = 0; i <= 2000; i = i + randomNumber(10, 100)) {
+            crc2.moveTo(i, 500);
+            crc2.lineTo(i, 2000);
+            crc2.strokeStyle = "#" + randomNumber(10, 20) + randomNumber(4, 7) + randomNumber(0, 9) + randomNumber(0, 9) + randomNumber(0, 9);
+            crc2.stroke();
+            crc2.closePath();
+        }
+        //Horizon
+        crc2.beginPath();
+        crc2.moveTo(0, 500);
+        crc2.lineTo(2000, 500);
+        crc2.strokeStyle = "#" + randomNumber(10, 20) + randomNumber(4, 7) + randomNumber(0, 9) + randomNumber(0, 9) + randomNumber(0, 9);
+        crc2.stroke();
         //Sun
-        let x = randomNumber(100, 1700);
-        let y = randomNumber(100, 900);
+        let x = randomNumber(200, 1300);
+        let y = randomNumber(100, 300);
         crc2.beginPath();
         crc2.arc(x, y, 200, 0, 2 * Math.PI);
         crc2.scale(10, 10);
--- 08Canvas1/Canvas.ts
+++ 08Canvas1/Canvas.ts
@@ -27,17 +27,46 @@ namespace Canvas01 {
             }  
 
 
+        //Grid
+
+        crc2.beginPath();
+        for ( let i: number = 0; i <= 2000; i = i + randomNumber(10, 100) ) {
+
+            crc2.moveTo(i, 500);
+            crc2.lineTo(i, 2000);
+
+            crc2.strokeStyle = "#" + randomNumber(10, 20) + randomNumber(4, 7) + randomNumber(0, 9) + randomNumber(0, 9) + randomNumber(0, 9);
+            crc2.stroke();
+            crc2.closePath();
+            
+            
+        }
+        
+        
+        //Horizon
+
+        crc2.beginPath();
+        crc2.moveTo(0, 500);
+        crc2.lineTo(2000, 500);
+        crc2.strokeStyle = "#" + randomNumber(10, 20) + randomNumber(4, 7) + randomNumber(0, 9) + randomNumber(0, 9) + randomNumber(0, 9);
+        crc2.stroke();
+
         
         //Sun
 
-        let x: number = randomNumber(100, 1700);
-        let y: number = randomNumber(100, 900);
+        let x: number = randomNumber(200, 1300);
+        let y: number = randomNumber(100, 300);
 
         crc2.beginPath();
         crc2.arc(x, y, 200, 0, 2 * Math.PI);
         crc2.scale(10, 10);
         crc2.fillStyle = "#" + "FF" + randomNumber(0, 9) + randomNumber(0, 9) + randomNumber(0, 9) + randomNumber(0, 9);
         crc2.fill();
+
+        
+        
+
+    
 
         
 
